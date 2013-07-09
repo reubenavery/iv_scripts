eac80ec7860572075fc303b1fb58b906245b6aed
Reuben Avery <reuben.avery@nbcuni.com>
2013-07-09 17:27:23 -0400

    Enhancing redirects processor, bugfixes

--- redirects.php
+++ redirects.php
@@ -1,25 +1,59 @@
 #!/usr/bin/env /Applications/MAMP/bin/php/php5.3.20/bin/php
 <?php
 
+// Hardcoded arguments for now:
+
+$filename = '7-3-13 Batch.xlsx';
+$ignore_hosts = true;
+$skip_first = true;
+
+// /args
+
+if (!file_exists($filename)) {
+  die("\n". 'Could not open '. $filename ."\n");
+}
+
+$ext = pathinfo($filename, PATHINFO_EXTENSION);
+
 require_once './includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 
 function rewrite_quote($str) {
-  return str_replace(' ', '\ ', preg_quote($str, '/'));
+  $str = str_replace(' ', '\ ', preg_quote($str, '/'));
+  $str = preg_replace('/(?:%20|\+|\ )/', '(?:%20|\+|\ )', $str);
+  return $str;
 }
 
 ini_set('auto_detect_line_endings', true);
 
-$opts = getopt("");
-
-$ignore_hosts = true;
-
-if (true) {
+if ($filename) {
   db_query("truncate redirects");
-  $lines = file('301 Redirect 6-25.txt');
+
+  if ($ext == 'xlsx') {
+    require_once 'simplexlsx.class.php';
+    $xlsx = new SimpleXLSX($filename);
+    $lines = $xlsx->rows();
+  }
+  else {
+    $lines = file($filename);
+  }
+
   $i = 0;
   foreach ($lines as $line) {
-    $parsed =  str_getcsv(trim($line), "\t", '"');
+    if ($skip_first) {
+      $skip_first = false;
+      continue;
+    }
+
+    switch ($ext) {
+      case 'xlsx':
+        $parsed = $line;
+        break;
+      default:
+        // default file format is csv
+        $parsed =  str_getcsv(trim($line), "\t", '"');
+    }
+
     $i++;
 
     $url = parse_url($parsed[0]);
@@ -36,7 +70,7 @@ if (true) {
     $new_query = $url['query'];
     $new_fragment = $url['fragment'];
 
-    $response_code = 301;
+    $response_code = (is_numeric($parsed[2])) ? $parsed[2] : 301;
 
     $data = array(
       $old_host,
@@ -64,7 +98,7 @@ while ($row = db_fetch_array($query)) {
   extract($row);
 
   if ($old_host !== $last_host && !$ignore_hosts) {
-    print "\n";
+//    print "\n";
     print 'RewriteCond %{HTTP_HOST} ^'. rewrite_quote($old_host) ."$ [NC]\n";
     $last_host = $old_host;
     $last_query = FALSE;
@@ -72,7 +106,7 @@ while ($row = db_fetch_array($query)) {
 
   if ($old_query !== $last_query) {
     if ($old_query) {
-      print "\n";
+//      print "\n";
       if (!$ignore_hosts) { print 'RewriteCond %{HTTP_HOST} ^'. rewrite_quote($old_host) ."$ [NC]\n"; }
       print 'RewriteCond %{QUERY_STRING} ^'. rewrite_quote($old_query, '/') ."$\n";
     }
@@ -85,9 +119,9 @@ while ($row = db_fetch_array($query)) {
   $response_code = ($response_code) ? '='. $response_code : '';
 
   if ($ignore_hosts) {
-    print 'RewriteRule ^/'. rewrite_quote($old_path, '/') .'$ /'. $new_path .' [R'. $response_code .',L]'. "\n";
+    print 'RewriteRule ^\/??'. rewrite_quote($old_path, '/') .'$ /'. $new_path .' [R'. $response_code .',L]'. "\n";
   }
   else {
-    print 'RewriteRule ^/'. rewrite_quote($old_path, '/') .'$ http://'. $new_host .'/'. $new_path .' [R'. $response_code .',L]'. "\n";
+    print 'RewriteRule ^\/??'. rewrite_quote($old_path, '/') .'$ http://'. $new_host .'/'. $new_path .' [R'. $response_code .',L]'. "\n";
   }
 }
